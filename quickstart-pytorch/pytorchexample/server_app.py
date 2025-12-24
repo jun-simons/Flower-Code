cf9b56ab810b82c068eeccff134e32797fc01cf4
Jun Simons <jdsimons017@gmail.com>
2025-12-23 23:30:56 -0500

    Test FedAdagrad strat

--- quickstart-pytorch/pytorchexample/server_app.py
+++ quickstart-pytorch/pytorchexample/server_app.py
@@ -3,7 +3,7 @@
 import torch
 from flwr.app import ArrayRecord, ConfigRecord, Context, MetricRecord
 from flwr.serverapp import Grid, ServerApp
-from flwr.serverapp.strategy import FedAvg
+from flwr.serverapp.strategy import FedAdagrad
 
 from pytorchexample.task import Net, load_centralized_dataset, test
 
@@ -25,7 +25,7 @@ def main(grid: Grid, context: Context) -> None:
     arrays = ArrayRecord(global_model.state_dict())
 
     # Initialize FedAvg strategy
-    strategy = FedAvg(fraction_evaluate=fraction_evaluate)
+    strategy = FedAdagrad(fraction_evaluate=fraction_evaluate)
 
     # Start strategy, run FedAvg for `num_rounds`
     result = strategy.start(
