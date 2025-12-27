4f7c24d0904780bd4dd1e2bd0725d743d3a6bb6d
Jun Simons <jdsimons017@gmail.com>
2025-12-27 16:03:52 -0500

    implement LR decay override

--- custom_strategy.py
+++ custom_strategy.py
@@ -5,4 +5,12 @@ from flwr.app import ArrayRecord, ConfigRecord, Message
 
 
 class CustomFedAdagrad(FedAdagrad):
-    def configure_train():
+    def configure_train(
+        self, server_round: int, arrays: ArrayRecord, config: ConfigRecord, grid: Grid
+    ) -> Iterable[Message]:
+        """Configure next round of training with LR decay"""
+        if server_round % 5 == 0 and server_round > 0:
+            config["learning-rate"] *= 0.5
+            print("LR decreased to:", config["learning-rate"])
+        return super().configure_train(server_round, arrays, config, grid)
+
